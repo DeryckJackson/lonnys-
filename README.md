f6a982f74a3258a29839424d0b826cdf91e514d7
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-13 10:08:39 -0700

    create working first logic
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- README.md
+++ README.md
@@ -10,4 +10,19 @@ Output ; pong
 
 Behavior 3 : All numbers divisable by both 3 and 5 are replaced by ping-pong
 Input : 15
-Output : ping-pong
+Output : 
+1
+2
+"ping"
+4
+"pong"
+"ping"
+7
+8
+"ping"
+"pong"
+11
+"ping"
+13
+14
+"ping-pong"
--- ping-pong.cs
+++ ping-pong.cs
@@ -4,6 +4,16 @@ class PingPong
 {
   static void Main()
   {
-    
+    Console.WriteLine("Please input a number to count to:");
+    int number = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i < number; i++) 
+    {
+      if ((i % 3) == 0)
+      {
+        Console.WriteLine("ping");
+      }
+      Console.WriteLine(i.ToString());
+    }
   }
 }
